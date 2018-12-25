62751a19f86f3fca2fd98059846c3c8b327d5891
M Salman Saeed Ch <36570180+msalmansaeedch786@users.noreply.github.com>
2018-12-25 16:33:00 +0500

    msalmansaeedch
    updated compiler engine api code to support the multi-users.

---
+++ CompilerNetwork/User/bsef15a018/Batch.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Automata\source\repos\CodeFreak\CompilerNetwork\User\bsef15a018
+g++ Code.cpp -o Code.exe
+Code.exe
---
+++ CompilerNetwork/User/bsef15a025/Batch.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Automata\source\repos\CodeFreak\CompilerNetwork\User\bsef15a025
+g++ Code.cpp -o Code.exe
+Code.exe
