4168bae06a8a1dcc543abd71de7a71aedeedaa6d
M Salman Saeed Ch <36570180+msalmansaeedch786@users.noreply.github.com>
2019-01-22 20:06:13 +0500

    msalmansaeedch
    fully featured compiler engine api, tested with database data.

--- CodeFreak1/Models/DBCodeFreakContext.cs
+++ CodeFreak1/Models/DBCodeFreakContext.cs
@@ -37,7 +37,7 @@ namespace CodeFreak1.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server =.; Database = DBCodeFreak; User Id = sa; Password = 123456; MultipleActiveResultSets = true");
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-8M5EN65;Initial Catalog=DBCodeFreak;Integrated Security=True");
             }
         }
 
