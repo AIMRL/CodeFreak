cac98a3a7bdcb856c85a943859f58146e364b341
M Salman Saeed Ch <36570180+msalmansaeedch786@users.noreply.github.com>
2019-02-13 00:15:53 +0500

    msalmansaeedch
    Compiler API complete with compile time, TLE and memory exceed errors.

--- CodeFreak1/ClientApp/src/app/AppSetting.ts
+++ CodeFreak1/ClientApp/src/app/AppSetting.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
 
-  public static baseUrl: string = 'https://localhost:44332/';
+  public static baseUrl: string = 'https://localhost:44394/';
 
   //Controller Path
   public static authURl: string = `/api/Auth/`; 
